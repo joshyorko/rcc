3b06eb4e7bc3d5d7da401273cfd410a4619dfae6
Josh Yorko <54248591+joshyorko@users.noreply.github.com>
2026-01-17 08:51:51 +0000

    fix: update Micromamba version to v2.5.0 and adjust version limit accordingly

--- blobs/embedded.go
+++ blobs/embedded.go
@@ -8,7 +8,7 @@ import (
 const (
 	// for micromamba upgrade, change following constants to match
 	// and also remember to update assets/micromamba_version.txt to match this
-	MicromambaVersionLimit = 1_005_011
+	MicromambaVersionLimit = 2_005_000
 )
 
 //go:embed assets/*.yaml docs/*.md
