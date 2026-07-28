1e5bb934c9c0de3d5e59a3fa57c834e967bc0ca1
joshyorko <joshua.yorko@gmail.com>
2026-07-28 08:18:45 -0400

    fix(init): simplify product initialization logic

--- common/variables.go
+++ common/variables.go
@@ -82,13 +82,7 @@ func init() {
 	args := set.Set(lowargs)
 	WarrantyVoidedFlag = set.Member(args, "--warranty-voided")
 	BundledFlag = set.Member(args, "--bundled")
-	robocorp := set.Member(args, "--robocorp")
-	switch {
-	case robocorp:
-		Product = LegacyMode()
-	default:
-		Product = LegacyMode()
-	}
+	Product = LegacyMode()
 	NoTempManagement = set.Member(args, "--no-temp-management")
 	NoPycManagement = set.Member(args, "--no-pyc-management")
 	if set.Member(args, "--debug") {
